452e598d9115ea4b77d0527017e51d69a5b4a79b
Kirill Chaialo <fotonka098@gmail.com>
2022-12-28 19:18:56 +0100

    V2

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,21 +1,7 @@
-// export function fetchCountries(name) {
-//   const url = `https://restcountries.com/v3.1/${name}?fields=name,capital,population,flags,languages`;
-
-//   return fetch(url)
-//     .then(response => response.json())
-//     .then(data => {
-//       console.log('data', data);
-//     })
-//     .catch(error => {
-//       console.log('error', error);
-//     });
-// }
-
 const url = 'https://restcountries.com/v3.1/name/';
-const fields = 'fields=name,capital,population,flags,languages';
 
 export function fetchCountries(name) {
-  return fetch(`${url}${name}?${fields}`)
+  return fetch(`${url}${name}`)
     .then(response => response.json())
     .catch(error => console.log(error));
 }
--- src/index.js
+++ src/index.js
@@ -3,76 +3,77 @@ import debounce from 'lodash.debounce';
 import Notiflix from 'notiflix';
 import { fetchCountries } from './fetchCountries';
 
-const DEBOUNCE_DELAY = 300;
+const DEBOUNCE_DELAY = 500;
 
 const inputRef = document.querySelector('#search-box');
 const countryList = document.querySelector('.country-list');
-const countryInfoRef = document.querySelector('.country-info');
+const countryInfo = document.querySelector('.country-info');
 
 inputRef.addEventListener('input', debounce(onInput, DEBOUNCE_DELAY));
 
-function tooManyCountries() {
+const failedInput = () => {
+  Notiflix.Notify.failure('Oops, there is no country with that name');
+};
+
+const tooManyCountries = () => {
   Notiflix.Notify.info(
     'Too many matches found. Please enter a more specific name.'
   );
-}
+};
 
-function failedInput() {
-  Notiflix.Notify.failure('Oops, there is no country with that name');
-}
+const rendercountryListMarkup = country => {
+  return country
+    .map(({ name, flags }) => {
+      return ` <li class="country-list__li">
+		<img class="country-list__flag" src="${flags.svg}" alt="Flag of ${name.official}" width = 30px height = 30px>
+		<h2 class="country-list__name">${name.official}</h2>
+	</li>`;
+    })
+    .join('');
+};
+
+const rendercountryInfoMarkup = country => {
+  return country
+    .map(({ capital, population, languages }) => {
+      return `<ul class="country-info__list">
+		<li class="country-info__li"><p><b>Capital: </b>${capital}</p></li>
+		<li class="country-info__li"><p><b>Population: </b>${population}</p></li>
+		<li class="country-info__li"><p><b>Languages: </b>${Object.values(
+      languages
+    ).join(', ')}</p></li>
+</ul>`;
+    })
+    .join('');
+};
 
 function onInput() {
   const name = inputRef.value.trim();
   if (name === '') {
-    return (countryList.innerHTML = ''), (countryInfoRef.innerHTML = '');
+    countryInfo.innerHTML = '';
+    countryList.innerHTML = '';
   }
 
   fetchCountries(name)
     .then(countries => {
+      countryInfo.innerHTML = '';
       countryList.innerHTML = '';
-      countryInfoRef.innerHTML = '';
       if (countries.length === 1) {
-        countryList.insertAdjacentHTML('beforeend', renderMarkup(countries));
-        countryInfoRef.insertAdjacentHTML(
+        countryList.insertAdjacentHTML(
           'beforeend',
-          renderCountryInfoRef(countries)
+          rendercountryListMarkup(countries)
+        );
+        countryInfo.insertAdjacentHTML(
+          'beforeend',
+          rendercountryInfoMarkup(countries)
         );
       } else if (countries.length >= 10) {
         tooManyCountries();
       } else {
-        countryList.insertAdjacentHTML('beforeend', renderMarkup(countries));
+        countryList.insertAdjacentHTML(
+          'beforeend',
+          rendercountryListMarkup(countries)
+        );
       }
     })
     .catch(failedInput);
-}
-
-function renderMarkup(countries) {
-  const markup = countries
-    .map(({ name, flags }) => {
-      return `
-          <li class="country-list__li">
-              <img class="country-list__flag" src="${flags.svg}" alt="Flag of ${name.official}" width = 30px height = 30px>
-              <h2 class="country-list__name">${name.official}</h2>
-          </li>
-          `;
-    })
-    .join('');
-  return markup;
-}
-
-function renderCountryInfoRef(countries) {
-  const markup = countries
-    .map(({ capital, population, languages }) => {
-      return `
-        <ul class="country-info__list">
-            <li class="country-info__li"><p><b>Capital: </b>${capital}</p></li>
-            <li class="country-info__li"><p><b>Population: </b>${population}</p></li>
-            <li class="country-info__li"><p><b>Languages: </b>${Object.values(
-              languages
-            ).join(', ')}</p></li>
-        </ul>
-        `;
-    })
-    .join('');
-  return markup;
 }
